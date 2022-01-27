fe6771ff0514f14f8ad8ca995e75e4afa95db5a3
tyronmaster <degonski.art@gmail.com>
2022-01-27 08:07:52 +0200

    feat: add task solving

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution there
+
+  let config = {};
+  let stack = [];
+  bracketsConfig.forEach(element => { config[element[1]] = element[0] });
+  console.log(config);
+  for (let i = 0; i < str.length; i++) {
+    if (Object.values(config).indexOf(str[i]) > -1) {
+      if (Object.keys(config).indexOf(str[i]) > -1 && str[i] === stack[stack.length - 1]) {
+        stack.pop();
+      } else {
+        stack.push(str[i]);
+      }
+    } else
+      if (config[str[i]] !== stack.pop()) {
+        return false;
+      }
+  }
+  return stack.length === 0;
 }
